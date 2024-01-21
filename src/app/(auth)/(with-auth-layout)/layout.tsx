94e01539a7ca77a0b04c62731c5638573bc90332
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-21 21:31:19 +0500

    route group layout

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,10 @@
+export default function AuthLayout({
+    children
+}: { children: React.ReactNode }) {
+    return (
+        <div>
+            <h2>Inner layout</h2>
+            {children}
+        </div>
+    )
+}
