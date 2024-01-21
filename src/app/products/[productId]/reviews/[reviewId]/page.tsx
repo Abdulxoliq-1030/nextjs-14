668afdba856ab04aabc14877c3f29c5959bbbe5d
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-21 16:39:40 +0500

    404 not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { notFound } from 'next/navigation'
 
 const ReviewDetail = ({ params }: {
     params: {
@@ -6,6 +7,9 @@ const ReviewDetail = ({ params }: {
         reviewId: string
     }
 }) => {
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
     return (
         <h1>Review {params.reviewId} for product {params.productId}</h1>
     )
