21060dfc80222ec286fad0cc15a0209574c43e4d
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-21 14:37:09 +0500

    dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDetails = ({ params }: { params: { productId: string } }) => {
+    return (
+        <h1>Details about product {params.productId}</h1>
+    )
+}
+
+export default ProductDetails
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductList = () => {
+    return (
+        <>
+            <h1>Product List</h1>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+        </>
+    )
+}
+
+export default ProductList
