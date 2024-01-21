22b0eef4e8b215ef16d806605d80d9279bfa0933
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-21 21:45:18 +0500

    generate routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+export const metadata = {
+    title: "About Abdukholiq"
+}
+
 const About = () => {
     return (
         <h1>About</h1>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { Metadata } from 'next'
 
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`Samsung S${params.productId}`)
+        }, 100);
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
+
+const ProductDetails = ({ params }: Props) => {
     return (
         <h1>Details about product {params.productId}</h1>
     )
