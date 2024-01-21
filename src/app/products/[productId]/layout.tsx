d9fb15a8a1f291de702f20c453c3a7ddf72eb064
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-21 21:24:32 +0500

    nested layouts

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ProductDetailsLayout({
+    children,
+}: { children: React.ReactNode }) {
+    return (
+        <>
+            {children}
+            <h2>Features products</h2>
+        </>
+    )
+}
