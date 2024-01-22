b2373ee753624f7525ae308d22153c293a9d4e5b
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-01-22 11:48:46 +0500

    link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link"
+
 export default function Home() {
-    return <h1>Welcome Home!</h1>
+    return <>
+        <h1>Welcome Home!</h1>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Products</Link>
+    </>
 }
